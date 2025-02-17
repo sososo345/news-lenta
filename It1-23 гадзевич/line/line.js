4cbbabe359fb88fa5aa2af7eb5352453650bdd17
sososo345 <zh.gadzevich@mail.ru>
2025-02-17 16:39:33 +0300

    Add files via upload

---
+++ It1-23 гадзевич/line/line.js
@@ -0,0 +1,16 @@
+document.getElementById("myprofbut").addEventListener('click', function(){
+    window.open('../my profile/user.html')
+});
+document.getElementById("like1").addEventListener('click', function(){
+    this.className="like-liked"
+});
+document.getElementById("like2").addEventListener('click', function(){
+    this.className="like-liked"
+});
+document.getElementById("like3").addEventListener('click', function(){
+    this.className="like-liked"
+});
+document.getElementById("like4").addEventListener('click', function(){
+    this.className="like-liked"
+});
+
