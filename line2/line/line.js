7ac043b728525a662a0eea7534fcb559d7464de6
sososo345 <zh.gadzevich@mail.ru>
2025-03-03 16:42:56 +0300

    Add files via upload

--- line2/line/line.js
+++ line2/line/line.js
@@ -14,5 +14,25 @@ document.getElementById("like4").addEventListener('click', function(){
     this.className="like-liked"
 });
 
-document.addEventListener*()
+LoadNews()
+async function LoadNews() {
+    const responce = await fetch("news.json")
+    const News = await responce.json()
+    const NewsFeed = document.getElementById('news')
+    News.ForEach(item=>{
+        const New =document.createElement('div')
+        New.className='newsitem'
+        const newhead = document.createElement('div')
+        newhead.textContent= item.title
+        New.appendChild(newhead)
+        const newimg = document.createElement('img.src')
+        newimg.src= item.img
+        New.appendChild(newhead)
+        const newcont = document.createElement('div')
+        newcont.textContent= item.title
+        New.appendChild(newhead)
+        NewsFeed.appendChild(New)
+    })
+    
+}
 
